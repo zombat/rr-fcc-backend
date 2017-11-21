5dede6f50faa0e8e9c9e361c61342e96f4e316c8
Raymond <raymond.rizzo@gmail.com>
2017-11-21 17:46:37 -0500

    FB login test 4.

--- app/routes.js
+++ app/routes.js
@@ -16,6 +16,15 @@ module.exports = function (app, passport) {
 		
 		
 	
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 
 		
 		
